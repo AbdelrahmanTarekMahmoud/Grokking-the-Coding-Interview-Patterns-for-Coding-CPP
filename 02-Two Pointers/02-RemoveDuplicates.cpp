52a67818fc92a8a6f24d051e115d9f0c68907584
Abdelrahman Tarek Mahmoud <85140058+AbdelrahmanTarekMahmoud@users.noreply.github.com>
2022-11-09 03:05:57 +0200

    Create 02-RemoveDuplicates.cpp

---
+++ 02-Two Pointers/02-RemoveDuplicates.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) 
+    {
+     int nextNonDuplicate = 1;
+     for(int i =1;i<nums.size();i++)
+     {
+         if(nums[nextNonDuplicate-1] != nums[i])
+         {
+             nums[nextNonDuplicate] = nums[i];
+             nextNonDuplicate++;
+         }
+     }
+     return nextNonDuplicate;
+    }
+};
